f6ea2c52fbf2ee4d82fa58cb696f1cd985d7ca74
Mashud Ahmed <mashudahmed1980@gmail.com>
2019-10-31 00:07:04 -0400

    Initial Commit

--- Scripts/app.js
+++ Scripts/app.js
@@ -6,9 +6,12 @@
   // function is anonymous. Start is variable identifier, taking ref to function and assigning to identifier, like making a named function
   //let Start = function() {
   function Start() {
-    let myLocalVariable = 1;
-    console.log(`App Started ...${myFunctionalVariable}`);
+    console.log(
+      `%c App Started ...`,
+      "font-size: 20px; font-weight: bold; color: blue;"
+    );
   }
 
   window.addEventListener("load", Start);
+  //Start();
 })();
